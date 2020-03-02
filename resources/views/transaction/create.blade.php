6e1875c3f72e422b61d5e944c359cdbdbf455d97
priyafaza <saviergen@gmail.com>
2020-03-02 16:13:37 +0700

    Update Mogration, Const

--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -16,31 +16,15 @@
     <!-- App Capsule -->
     <div id="appCapsule">
         <div class="section full mt-1 mb-2">
-            <div class="section-title">Boxed</div>
+            <div class="section-title">Date : {{ now() }}</div>
             <div class="wide-block pb-1 pt-2">
 
                 <form>
                     <div class="form-group boxed">
                         <div class="input-wrapper">
-                            <label class="label" for="name5">Name</label>
-                            <input type="text" class="form-control" id="name5" placeholder="Enter your name">
-                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
-                        </div>
-                    </div>
-
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <label class="label" for="email5">E-mail</label>
-                            <input type="email" class="form-control" id="email5" placeholder="E-mail address">
-                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
-                        </div>
-                    </div>
-
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <label class="label" for="city5">City</label>
+                            <label class="label" for="city5">Pelanggan</label>
                             <select class="form-control custom-select" id="city5">
-                                <option value="0">Select a city</option>
+                                <option value="0">Pilih Pelanggan</option>
                                 <option value="1">New York City</option>
                                 <option value="2">Austin</option>
                                 <option value="3">Colorado</option>
@@ -50,33 +34,58 @@
 
                     <div class="form-group boxed">
                         <div class="input-wrapper">
-                            <label class="label" for="password5">Password</label>
-                            <input type="password" class="form-control" id="password5" placeholder="Type a password">
-                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
+                            <label class="label" for="city5">Product</label>
+                            <select class="form-control custom-select" id="city5">
+                                <option value="0">Pilih Product</option>
+                                <option value="1">New York City</option>
+                                <option value="2">Austin</option>
+                                <option value="3">Colorado</option>
+                            </select>
                         </div>
                     </div>
 
                     <div class="form-group boxed">
                         <div class="input-wrapper">
-                            <label class="label" for="phone5">Phone</label>
-                            <input type="tel" class="form-control" id="phone5" placeholder="Enter your phone number">
-                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
-                        </div>
-                    </div>
-
-                    <div class="form-group boxed">
-                        <div class="input-wrapper">
-                            <label class="label" for="address5">Address</label>
+                            <label class="label" for="address5">Deskripsi</label>
                             <textarea id="address5" rows="2" class="form-control"></textarea>
                             <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
                         </div>
                     </div>
+
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <label class="label" for="email5">Nominal</label>
+                            <input type="number" class="form-control" id="email5" placeholder="Nominal">
+                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
+                        </div>
+                    </div>
+
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <label class="label" for="password5">Harga Jual</label>
+                            <input type="number" class="form-control" id="password5" placeholder="Harga Jual">
+                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
+                        </div>
+                    </div>
+
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <label class="label" for="phone5">Harga Beli</label>
+                            <input type="number" class="form-control" id="phone5" placeholder="Harga Beli">
+                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group boxed">
+                        <div class="input-wrapper">
+                            <a href="" class="btn btn-primary btn-block btn-lg">Simpan</a>
+                            <i class="clear-input"><ion-icon name="close-circle"></ion-icon></i>
+                        </div>
+                    </div>
+
                 </form>
 
             </div>
-        </div>
-        <div class="section mt-2 mb-2">
-            <a href="" class="btn btn-primary btn-block btn-lg">Simpan</a>
         </div>
     </div>
     <!-- * App Capsule -->
