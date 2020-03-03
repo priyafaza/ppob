2bfc3d416e5b484b87735e1531b1950be86cfb61
ijlik <ijlik.k5s@gmail.com>
2020-03-03 11:32:09 +0700

    update bayar

--- resources/views/bill/detail.blade.php
+++ resources/views/bill/detail.blade.php
@@ -63,5 +63,9 @@
             <a href="https://api.whatsapp.com/send?phone={{ $pelanggan->no_hp}}&text=Halo {{ $pelanggan->username }},%0ASekedar mengingatkan bahwa per tanggal {{ now()->format('d F Y') }}, {{ $pelanggan->username }} sudah ngutang sebanyak {{ $jumlah }} kali dengan total tagihan sementara sebesar Rp. {{ number_format($total,0,'.',',') }}. Oke itu dulu, nanti kalo ada transaksi baru bakalan di kasih tau lagi. Terima Kasih.%0A%0AIjlik%0A_Pesan ini dibuat otomatis_" type="submit" class="btn btn-success btn-block btn-lg"><ion-icon name="logo-whatsapp"></ion-icon> Kirim Tagihan</a>
         </div>
 
+        <div class="section mt-2 mb-2">
+            <a href="https://api.whatsapp.com/send?phone={{ $pelanggan->no_hp}}&text=Halo {{ $pelanggan->username }},%0AIni informasi cara pembayaran via transfer.%0A%0ASilahkan transfer sebesar Rp. {{ number_format($total,0,'.',',') }} ke salah satu rekening bank berikut ini.%0A%0A - 0240633221 BCA A/n Adi Nugroho%0A - 0404822070 BNI A/n Adi Nugroho%0A - 90012874718 BTPN/Jenius A/n Adi Nugroho%0A%0AAtau bisa juga melalui E-Wallet berikut.%0A*OVO*, *GoPay*, *Dana*, *LinkAja* dengan nomor hp 085732860740.%0AUntuk pembayaran metode lain silahkan tanya langsung aja. Terima Kasih.%0A%0AIjlik%0A_Pesan ini dibuat otomatis_" type="submit" class="btn btn-success btn-block btn-lg"><ion-icon name="logo-whatsapp"></ion-icon> Kirim Cara Pembayaran</a>
+        </div>
+
     </div>
 @stop
